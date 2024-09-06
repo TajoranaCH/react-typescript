7067c5a2c6cf615776feefe6c83bab3eac89adf9
Japneet Singh <japneetsingh1807@outlook.it>
2024-09-06 17:42:37 +0200

    exc

--- react-with-types/src/App.tsx
+++ react-with-types/src/App.tsx
@@ -1,15 +1,13 @@
+import { CoursePart } from "./types";
+import { assertNever } from "./utils";
+
 interface HeaderProps {
   title: string;
 }
 
-interface CoursePart {
-  name: string,
-  exerciseCount: number
-}
-
-interface ContentProps {
+interface PartProps {
   courseParts: CoursePart[]
-}
+};
 
 interface TotalProps {
   total: number
@@ -19,10 +17,28 @@ const Header = (props: HeaderProps) => {
   return <h1>{props.title}</h1>
 }
 
-const Content = (props: ContentProps) => {
-  return props.courseParts.map(p => <p>
-    {p.name} {p.exerciseCount}
-  </p>)
+const Content = (props: PartProps) => {
+  return props.courseParts.map((p, i) => {
+    let partInfo;
+    switch(p.kind) {
+      case 'basic':
+        partInfo = <><i>{p.description}</i></>
+        break;
+      case 'group':
+        partInfo = <>project exercises: {p.groupProjectCount}</>;
+        break;
+      case 'background':
+        partInfo = <><p>{p.description}</p><p>{p.backgroundMaterial}</p></>;
+        break;
+      case 'special':
+        partInfo = <><p>{p.description}</p><p>requirements: {p.requirements.join(',')}</p></>;
+        break;
+      default:
+        assertNever(p);
+    }
+    return <div key={i}><h1>{p.name} {p.exerciseCount}</h1>{partInfo}<br /></div>;
+  }
+  )
 }
 
 const Total = (props: TotalProps) => {
@@ -33,20 +49,36 @@ const Total = (props: TotalProps) => {
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
-    },
+      exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic"    },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
-    },
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group"    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic"    },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+      kind: "background"    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      kind: "special"
     }
-  ];
+  ]
 
   const totalExercises = courseParts.reduce((sum, part) => sum + part.exerciseCount, 0);
 
